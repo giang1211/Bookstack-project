7e6b7943674db0306a5e72ddafd15cfa94ef8c2c
s8giang <s8giang@gmail.com>
2025-04-29 11:45:24 -0400

    update

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#! /bin/bash 
+
+echo " deploying the backend"
+
+kubens bookstack-s9
+kubectl apply  -f  bookstack_db_secret.yml
+kubectl apply  -f  bookstack_db_cm.yml
+kubectl apply  -f  bookstack_db_pvc-01.yml 
+kubectl apply  -f  bookstack_db_pvc-02.yml
+
+kubectl apply  -f  bookstack_db_svc.yml
+kubectl apply  -f  bookstack_db.yml
+
+
+kubectl apply  -f bookstack_frontend_cm.yml
+kubectl apply  -f bookstack_frontend_secret.yml
+kubectl apply  -f bookstack_frontend_pvc.yml
+kubectl apply  -f bookstack_frontend_svc.yml
+kubectl apply  -f bookstack_frontend.yml
+kubectl apply -f bookstack_env_cm.yml
+# kubectl apply  -f bookstack_ingress.yml
